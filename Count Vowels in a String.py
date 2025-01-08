e69d97a3ee12d74e0a7502291e2c90b7637718c7
bhanu_prakash <118003723+Bhanuvvxf@users.noreply.github.com>
2025-01-08 08:50:55 +0530

    Update Count Vowels in a String.py

--- Count Vowels in a String.py
+++ Count Vowels in a String.py
@@ -1,1 +1,3 @@
-#Count Vowels in a String
+#count vowels in a string 
+list = list(input("Enter a string : ").lower())
+print(f"Number of vowels: {list.count("a")+list.count("e")+list.count("i")+list.count("o")+list.count("u")}")
